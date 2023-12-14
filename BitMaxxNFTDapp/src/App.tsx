1d5ea5ece9ffeb2f602a484c733ab7660eed560b
ArielRin <degenftm@gmail.com>
2023-12-14 14:33:09 +1100

    ytmp4 fix

--- BitMaxxNFTDapp/src/App.tsx
+++ BitMaxxNFTDapp/src/App.tsx
@@ -25,19 +25,15 @@ import abiFile from './abiFile.json';
 import './styles.css';
 import backgroundGif from './sea.gif';
 import MainTextLogo from './headerlogo.png';
+import yourVideo from 'https://github.com/ArielRin/BitMaxx-NFT-Collection--BNFT/raw/final/1.mp4';
 
-import btm from './btm.png';
-const CONTRACT_ADDRESS = '0xaA0015FbB55b0f9E3dF74e0827a63099e4201E38'; // 2nd maxxtest
-// const CONTRACT_ADDRESS = '0x27B327315cb8EFBD671FDf82730a3bD25563aea5'; // first maxx test 2
-// const CONTRACT_ADDRESS = '0xeaD4A1507C4cEE75fc3691FA57b7f2774753482C'; // first maxx test 1
+const CONTRACT_ADDRESS = '0xeaD4A1507C4cEE75fc3691FA57b7f2774753482C';
 
 const getExplorerLink = () => `https://scan.maxxchain.org/address/${CONTRACT_ADDRESS}`;
 const getOpenSeaURL = () => `https://testnets.opensea.io/assets/goerli/${CONTRACT_ADDRESS}`;
 
 function App() {
   const account = useAccount();
-    console.log('Connected wallet address:', account);
-
   const [contractName, setContractName] = useState('');
   const [totalSupply, setTotalSupply] = useState(0);
   const [loading, setLoading] = useState(true);
@@ -94,22 +90,12 @@ function App() {
       setMintLoading(true);
       const totalPrice = calculateTotalPrice();
 
-      // Call the mint function in the contract
       const tx = await mint({
         args: [mintAmount, { value: totalPrice }],
       });
 
       await tx.wait();
       toast.success('Mint successful! You can view your NFT soon.');
-
-      // Update totalSupply after a successful mint
-      setTotalSupply((prevTotalSupply) => prevTotalSupply + mintAmount);
-
-      // Update contract balance after successful mint
-      const provider = new ethers.providers.JsonRpcProvider('https://mainrpc4.maxxchain.org/');
-      const contract = new ethers.Contract(CONTRACT_ADDRESS, abiFile, provider);
-      const balance = await contract.getContractBalance();
-      setContractBalanceValue(parseFloat(ethers.utils.formatEther(balance)));
     } catch (error) {
       console.error(error);
       toast.error('Mint unsuccessful! Please try again.');
@@ -120,50 +106,41 @@ function App() {
 
 
 
+  const onSetCostClick = async () => {
+    try {
+      // Convert the new cost value to Wei
+      const newCostValueInWei = ethers.utils.parseUnits(newCost.toString(), 'wei');
 
+      // Call the setCost function in the contract
+      const tx = await setNewCostFn({
+        args: [newCostValueInWei],
+      });
 
-const onSetCostClick = async () => {
-  try {
-    // Convert the new cost value to Wei
-    const newCostValueInWei = ethers.utils.parseUnits(newCost.toString(), 'wei');
-
-    // Call the setCost function in the contract
-    const tx = await setNewCostFn({
-      args: [newCostValueInWei],
-    });
-
-    await tx.wait();
-    toast.success('Cost updated successfully!');
-
-    // Update the cost state after a successful setCost operation
-    setCost(newCost.toString());
-  } catch (error) {
-    console.error(error);
-    toast.error('Failed to update cost. Please try again.');
-  }
-};
-
+      await tx.wait();
+      toast.success('Cost updated successfully!');
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to update cost. Please try again.');
+    }
+  };
 
   const onTogglePauseClick = async () => {
-  try {
-    // Toggle the pause state
-    const newState = !isPaused;
+    try {
+      // Toggle the pause state
+      const newState = !isPaused;
 
-    // Call the pause function in the contract
-    const tx = await pauseContract({
-      args: [newState],
-    });
+      // Call the pause function in the contract
+      const tx = await pauseContract({
+        args: [newState],
+      });
 
-    await tx.wait();
-    toast.success(`Contract is now ${newState ? 'paused' : 'resumed'}`);
-
-    // Update isPaused state after a successful pause/unpause
-    setIsPaused(newState);
-  } catch (error) {
-    console.error(error);
-    toast.error('Failed to toggle pause state. Please try again.');
-  }
-};
+      await tx.wait();
+      toast.success(`Contract is now ${newState ? 'paused' : 'resumed'}`);
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to toggle pause state. Please try again.');
+    }
+  };
 
 
 
@@ -182,23 +159,37 @@ const onSetCostClick = async () => {
         const name = await contract.name();
         const supply = await contract.totalSupply();
         setContractName(name);
-        setTotalSupply(supply.toNumber());// Fetch the user's balance
-      if (account) {
-        const userBalanceResult = await contract.balanceOf(account);
-        setUserBalance(userBalanceResult.toNumber());
+        setTotalSupply(supply.toNumber());
+      } catch (error) {
+        console.error('Error fetching contract data:', error);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.error('Error fetching contract data:', error);
-    } finally {
-      setLoading(false);
     }
-  }
 
-  fetchContractData();
-}, [account]);
+    fetchContractData();
+  }, []);
 
+  const [contractBalance, setContractBalance] = useState(0);
 
+  useEffect(() => {
+    async function fetchContractBalance() {
+      try {
+        const provider = new ethers.providers.JsonRpcProvider('https://mainrpc4.maxxchain.org/');
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, abiFile, provider);
 
+        // Read the balance directly from the contract address
+        const balance = await provider.getBalance(CONTRACT_ADDRESS);
+
+        // Convert BigNumber to number before setting the state
+        setContractBalance(balance.toNumber());
+      } catch (error) {
+        console.error('Error fetching contract balance:', error);
+      }
+    }
+
+    fetchContractBalance();
+  }, []);
 
 const [cost, setCost] = useState('0');
 
@@ -241,8 +232,6 @@ useEffect(() => {
   fetchPauseStatus();
 }, []);
 
-
-
 const [isRevealed, setIsRevealed] = useState(false);
 
   useEffect(() => {
@@ -262,39 +251,6 @@ const [isRevealed, setIsRevealed] = useState(false);
 
     fetchRevealStatus();
   }, []);
-
-
-
-  const { writeAsync: withdrawFunds, error: withdrawError } = useContractWrite({
-    addressOrName: CONTRACT_ADDRESS,
-    contractInterface: abiFile,
-    functionName: 'withdraw',
-  });
-
-  const [withdrawLoading, setWithdrawLoading] = useState(false);
-
-  const onWithdrawClick = async () => {
-    try {
-      setWithdrawLoading(true);
-
-      // Call the withdraw function in the contract
-      const tx = await withdrawFunds();
-
-      await tx.wait();
-      toast.success('Funds withdrawn successfully!');
-
-      // Update contract balance after successful withdrawal
-      const provider = new ethers.providers.JsonRpcProvider('https://mainrpc4.maxxchain.org/');
-      const contract = new ethers.Contract(CONTRACT_ADDRESS, abiFile, provider);
-      const balance = await contract.getContractBalance();
-      setContractBalanceValue(parseFloat(ethers.utils.formatEther(balance)));
-    } catch (error) {
-      console.error(error);
-      toast.error('Failed to withdraw funds. Please try again.');
-    } finally {
-      setWithdrawLoading(false);
-    }
-  };
 
 
 
@@ -323,57 +279,8 @@ const [isRevealed, setIsRevealed] = useState(false);
     }
   };
 
-  const [contractBalanceValue, setContractBalanceValue] = useState(0);
 
-  useEffect(() => {
-    async function fetchContractBalance() {
-      try {
-        const provider = new ethers.providers.JsonRpcProvider('https://mainrpc4.maxxchain.org/');
-        const contract = new ethers.Contract(CONTRACT_ADDRESS, abiFile, provider);
 
-        // Read the contract balance directly from the contract
-        const balance = await contract.getContractBalance();
-
-        // Convert Wei to Ether and set the state
-setContractBalanceValue(parseFloat(ethers.utils.formatEther(balance)));      } catch (error) {
-        console.error('Error fetching contract balance:', error);
-      }
-    }
-
-    fetchContractBalance();
-  }, []);
-
-  const [userBalance, setUserBalance] = useState(0);
-
-const [userNftBalance, setUserNftBalance] = useState(0);
-
-useEffect(() => {
-  async function fetchUserNftBalance() {
-    try {
-      // Extract the Ethereum address from the account object
-      const userAddress = account.address;
-
-      if (userAddress && ethers.utils.isAddress(userAddress)) {
-        const provider = new ethers.providers.JsonRpcProvider('https://mainrpc4.maxxchain.org/');
-        const contract = new ethers.Contract(CONTRACT_ADDRESS, abiFile, provider);
-
-        // Call the balanceOf function to get the user's NFT balance
-        const userBalanceResult = await contract.balanceOf(userAddress);
-        setUserNftBalance(userBalanceResult.toNumber());
-      } else {
-        // Handle the case where the extracted address is not a valid address
-        console.error('Invalid or undefined Ethereum address:', userAddress);
-      }
-    } catch (error) {
-      console.error('Error fetching user NFT balance:', error);
-    }
-  }
-
-  // Fetch user's NFT balance only if the account is valid
-  if (account && account.address) {
-    fetchUserNftBalance();
-  }
-}, [account]);
 
 
   return (
@@ -409,7 +316,18 @@ useEffect(() => {
               <TabPanel>
 
               <div>
-                <img src={btm} alt="BitMaxx NFT Collection" className="logobody" />
+                <img src={MainTextLogo} alt="BitMaxx NFT Collection" className="logobody" />
+                <img src={MainTextLogo} alt="BitMaxx NFT Collection" className="logobody" />
+
+                    {/* Display the video in the Home tab */}
+                    <video controls width="100%" height="auto">
+                      <source
+                        src="https://bafybeiflhkmfkwfwnvu4ek3cwu57vkkz6x3rrnrmpyo7yd6zql5m5is6ny.ipfs.dweb.link/1.mp4"
+                        type="video/mp4"
+                      />
+                      Your browser does not support the video tag.
+                    </video>
+
 
                 <Text className="pricecost" style={{ textAlign: 'center', fontWeight: 'bolder' }}>
                   BitMaxx About our NFTs
@@ -433,83 +351,77 @@ useEffect(() => {
 
               </TabPanel>
               <TabPanel>
-  <div>
-    <img src={btm} alt="BitMaxx NFT Collection" className="logobody" />
-    <Text className="contractname" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
-      {loading ? 'Loading...' : `${contractName || 'N/A'}`}
-    </Text>
-    <Text className="totalSupply" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
-      {loading ? 'Loading...' : `Sold : ${totalSupply} / ${maxSupply}  `}
-    </Text>
-    <Text className="remainingSupply" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
-      {loading ? 'Loading...' : `Remaining Supply: ${remainingSupply}`}
-    </Text>
-    <Text className="contractaddr" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
-      <Link isExternal href={getExplorerLink()}>
-        {CONTRACT_ADDRESS}
-      </Link>
-    </Text>
-    <Text className="userNftBalance" style={{ textAlign: 'center', fontWeight: 'bold' }}>
-      {loading ? 'Loading...' : `BitMaxx NFTs in wallet : ${userNftBalance} `}
-    </Text>
-  </div>
 
-  {totalSupply === maxSupply ? (
-    <Text className="soldOutMessage" style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '3xl', color: 'red' }}>
-      Sold Out!
-    </Text>
-  ) : (
-    <>
-      <Text className="pricecost" style={{ textAlign: 'center', fontWeight: 'bolder' }}>
-        {cost} PWR Each!
-      </Text>
-      <Box marginTop='4' display='flex' alignItems='center' justifyContent='center'>
-        <Button
-          marginTop='1'
-          textColor='white'
-          bg='#058ba1'
-          _hover={{
-            bg: '#4d9795',
-          }}
-          onClick={handleDecrement}
-          disabled={!isConnected || mintLoading || mintAmount === 1}
-        >
-          -
-        </Button>
-        <Text marginX='3' textAlign='center' fontSize='lg'>
-          {mintAmount}
-        </Text>
-        <Button
-          marginTop='1'
-          textColor='white'
-          bg='#058ba1'
-          _hover={{
-            bg: '#4d9795',
-          }}
-          onClick={handleIncrement}
-          disabled={!isConnected || mintLoading || mintAmount === 200}
-        >
-          +
-        </Button>
-      </Box>
-      <Box marginTop='2' display='flex' alignItems='center' justifyContent='center'>
-        <Button
-          disabled={!isConnected || mintLoading}
-          marginTop='6'
-          onClick={onMintClick}
-          textColor='white'
-          bg='#058ba1'
-          _hover={{
-            bg: '#4d9795',
-          }}
-        >
-          {isConnected ? `Mint ${mintAmount} Now` : ' Mint on (Connect Wallet)'}
-        </Button>
-      </Box>
-    </>
-  )}
-</TabPanel>
+              <div>
+                <img src={MainTextLogo} alt="BitMaxx NFT Collection" className="logobody" />
+                <Text className="contractname" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
+                  {loading ? 'Loading...' : `${contractName || 'N/A'}`}
+                </Text>
+                <Text className="totalSupply" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
+                  {loading ? 'Loading...' : `Sold : ${totalSupply} / ${maxSupply}  `}
+                </Text>
+                <Text className="remainingSupply" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
+                  {loading ? 'Loading...' : `Remaining Supply: ${remainingSupply}`}
+                </Text>
+                <Text className="contractaddr" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
+                  <Link isExternal href={getExplorerLink()}>
+                    {CONTRACT_ADDRESS}
+                  </Link>
+                </Text>
+              </div>
 
+              <Text className="pricecost" style={{ textAlign: 'center', fontWeight: 'bolder' }}>
+                {cost} PWR Each!
+              </Text>
+              <Box marginTop='4' display='flex' alignItems='center' justifyContent='center'>
+                <Button
+                  marginTop='1'
+                  textColor='white'
+                  bg='#ffc114'
+                  _hover={{
+                    bg: '#ffdc39',
+                  }}
+                  onClick={handleDecrement}
+                  disabled={!isConnected || mintLoading || mintAmount === 1}
+                >
+                  -
+                </Button>
+                <Text marginX='3' textAlign='center' fontSize='lg'>
+                  {mintAmount}
+                </Text>
+                <Button
+                  marginTop='1'
+                  textColor='white'
+                  bg='#ffc114'
+                  _hover={{
+                    bg: '#ffdc39',
+                  }}
+                  onClick={handleIncrement}
+                  disabled={!isConnected || mintLoading || mintAmount === 200}
+                >
+                  +
+                </Button>
+              </Box>
+
+              <Box marginTop='2' display='flex' alignItems='center' justifyContent='center'>
+                <Button
+                  disabled={!isConnected || mintLoading}
+                  marginTop='6'
+                  onClick={onMintClick}
+                  textColor='white'
+                  bg='#ffc114'
+                  _hover={{
+                    bg: '#ffdc39',
+                  }}
+                >
+                  {isConnected ? `Mint ${mintAmount} Now` : ' Mint on (Connect Wallet)'}
+                </Button>
+              </Box>
+
+
+
+
+              </TabPanel>
               <TabPanel>
 
 
@@ -526,14 +438,13 @@ useEffect(() => {
 
 
                 <Text className="paragraph1" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
+                  {loading ? 'Loading...' : `Contract Balance: ${ethers.utils.formatEther(contractBalance)} PWR`}
+                </Text>
+                <Text className="paragraph1" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
                   {loading ? 'Loading...' : `Remaining Supply: ${remainingSupply}`}
                 </Text>
                 <Text className="paragraph1" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
                   {loading ? 'Loading...' : `NFT Price: ${cost} PWR`}
-                </Text>
-
-                <Text className="paragraph1" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
-                  {loading ? 'Loading...' : `Contract Balance: ${contractBalanceValue} PWR`}
                 </Text>
 
                 <Text className="pricecost" style={{ textAlign: 'center', fontWeight: 'bolder' }}>
@@ -544,9 +455,9 @@ useEffect(() => {
                   <Button
                     onClick={onRevealClick}
                     textColor="white"
-                    bg={isRevealed ? '#666' : '#058ba1'}
+                    bg={isRevealed ? '#666' : '#ff5555'}
                     _hover={{
-                      bg: isRevealed ? '#666' : '#4d9795',
+                      bg: isRevealed ? '#666' : '#ff6b6b',
                     }}
                     style={{ marginRight: '1rem' }}
                     >
@@ -556,9 +467,9 @@ useEffect(() => {
                   <Button
                     onClick={onTogglePauseClick}
                       textColor="white"
-                      bg="#058ba1"
+                      bg="#ff5555"
                       _hover={{
-                        bg: '#4d9795',
+                        bg: '#ff6b6b',
                       }}
                       >
                       {isPaused ? 'UnPause Minting ' : 'Pause Minting'}
@@ -590,28 +501,16 @@ useEffect(() => {
                 <Button
                  onClick={onSetCostClick}
                  textColor="white"
-                 bg="#058ba1"
+                 bg="#ff5555"
                  _hover={{
-                   bg: '#4d9795',
+                   bg: '#ff6b6b',
                  }}
                  >
                  Set Cost
                  </Button>
 
                </Box>
-                             <Box marginTop='2' display='flex' alignItems='center' justifyContent='center'>
-               <Button
-                                       onClick={onWithdrawClick}
-                                       textColor="white"
-                                       bg="#058ba1"
-                                       _hover={{
-                                         bg: '#4d9795',
-                                       }}
-                                     >
-                                       {withdrawLoading ? 'Withdrawing...' : 'Withdraw Funds (Only Owner)'}
-                                     </Button>
 
-                                                    </Box>
               </div>
 
 
@@ -632,11 +531,3 @@ useEffect(() => {
 }
 
 export default App;
-
-
-
-
-                //
-                // <Text className="paragraph1" style={{ padding: '10px', textAlign: 'center', fontWeight: 'bold' }}>
-                //   {loading ? 'Loading...' : `Contract Balance: ${ethers.utils.formatEther(contractBalance)} PWR`}
-                // </Text>
